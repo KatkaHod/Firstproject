d8af94b65b3d12e06f088077427a94af027fc955
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-01-22 20:04:17 +0100

    second class added

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        int lenghtA = 10;
+        int lenghtB = 5;
+        int RectangleArea = lenghtA * lenghtB;
+        System.out.print("The lenght of A is " + lenghtA + " and " + "The lenght of B is " + lenghtB + ".");
+        System.out.println("The lenght of the area is: " + RectangleArea);
+
+
+
+
+
+
+
+
+
+    }
+}
